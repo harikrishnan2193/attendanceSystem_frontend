d66be3c24b407b3292126e7fbb360a3e63bb81cd
harikrishnan2193 <harikrishnank2193@gmail.com>
2025-09-29 13:21:21 +0530

    update apiUrl in enveronments.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // apiUrl: 'https://attendancesystem-backend-zevg.onrender.com',
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://attendancesystem-backend-zevg.onrender.com',
+  // apiUrl: 'http://localhost:5000',
   googleClientId: '616175441869-b7vr136fcmvtkr1qsvkn4s5aoqs7h7a7.apps.googleusercontent.com',
 };
